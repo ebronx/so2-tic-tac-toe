59df7912715d8f60f33d0ee4cd5b05152143bc32
Masia <kindofmydream@gmail.com>
2023-05-29 14:37:53 +0200

    4 commmit

--- studentB.py
+++ studentB.py
@@ -1,4 +1,6 @@
 import random
+import time
+
 
 def ai_move(myboard):
     print("Get AI move")
@@ -15,7 +17,9 @@ def get_user_move(myboard):
     print("0   1   2")
     print("3   4   5")
     print("6   7   8")
-    while True:
+
+    start = time.time()
+    while (time.time() - start) < 5:
         while True:
             choice = int(input("Insert number from 0 to 8"))
             if choice in options:
@@ -27,6 +31,9 @@ def get_user_move(myboard):
             return choice
         else:
             print("Ta część planszy jest już zajęta")
+
+    if choice is None:
+        print("Czas na wprowadzenie wartości minął.")
 
 
 
